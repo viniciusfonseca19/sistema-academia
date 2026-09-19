2a9af0b633a0b306679359150502f4f63cfcfaff
Vinícius F. <fonsecavinicius117@gmail.com>
2026-09-18 21:41:32 -0300

    docs: adiciona exemplos didáticos de query methods e native query

--- src/main/java/br/com/vini/sistema_academia/database/repository/IExerciciosRepository.java
+++ src/main/java/br/com/vini/sistema_academia/database/repository/IExerciciosRepository.java
@@ -2,10 +2,27 @@ package br.com.vini.sistema_academia.database.repository;
 
 import br.com.vini.sistema_academia.database.model.ExerciciosEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface IExerciciosRepository extends JpaRepository<ExerciciosEntity, Integer> {
 
     public List<ExerciciosEntity> findAllByGrupoMuscular(String grupoMuscular);
+
+//    @Query(value = """
+//            SELECT e
+//            FROM ExerciciosEntity e
+//            WHERE UPPER(e.grupoMuscular) = UPPER(:grupoMuscular)
+//            """)
+//    List<ExerciciosEntity> findAllByGrupoMuscularJpql(@Param("grupoMuscular") String grupoMuscular);
+//
+//    @NativeQuery(value = """
+//        SELECT e
+//        FROM exercicios e
+//        WHERE UPPER(e.grupo_muscular) = UPPER(:grupoMuscular)
+//    """)
+//    List<ExerciciosEntity> findAllByGrupoMuscularNative(@Param("grupoMuscular") String grupoMuscular);
 }
